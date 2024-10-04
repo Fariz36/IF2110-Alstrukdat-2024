d908ddd75d0663e619b9a1519f85d85c813d5d5c
Fariz36 <faris361707@gmail.com>
2024-10-04 19:02:02 +0700

    edited by: 1

---
+++ Pra-Praktikum 1/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+
+int main() {
+  POINT P1, P2;
+  BacaPOINT(&P1);
+  Putar(&P1, 30); 
+  TulisPOINT(P1);
+  return 0;
+}
---
+++ Praktikum 0/testing.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  unsigned int x = 10;
+  while (x >= 0) {
+    printf("%u\n", x);
+    x--;
+  }
+}
